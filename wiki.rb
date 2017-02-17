14b31a396932bc30e3ad98edf21ba766cf91bb7a
Sam Brannon <sam@reverb.com>
2017-02-16 21:53:40 -0600

    Added edit functionality

--- wiki.rb
+++ wiki.rb
@@ -27,7 +27,18 @@ get "/:title" do
   erb :show
 end
 
+get "/:title/edit" do
+  @title = params[:title]
+  @content = page_content(@title)
+  erb :edit
+end
+
 post "/create" do
+  save_content(params['title'], params['content'])
+  redirect URI.escape("/#{params['title']}")
+end
+
+put "/:title" do
   save_content(params['title'], params['content'])
   redirect URI.escape("/#{params['title']}")
 end
